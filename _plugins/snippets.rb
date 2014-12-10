405ab1cf45095f9ba8c2376debbd98950691fab4
Mike Bland <michael.bland@gsa.gov>
2014-12-10 10:17:34 -0500

    Convert snippets guidelines to Markdown

--- _plugins/snippets.rb
+++ _plugins/snippets.rb
@@ -6,7 +6,6 @@ module Hub
         generate_snippets_page(site, timestamp, snippets)
       end
       generate_snippets_index_page(site)
-      generate_snippets_guidelines_page(site)
     end
 
     def self.generate_snippets_page(site, timestamp, snippets)
@@ -28,16 +27,6 @@ module Hub
 
       page.data['snippets'] = snippets
       page.data['members'] = site.data['snippets_team_members']
-      site.pages << page
-    end
-
-    # This generates the snippets/guidelines page. Though the content of the
-    # snippets_guidelines.html is all static, generating it this way rather
-    # than storing it in /pages keeps it from appearing when no snippets are
-    # present.
-    def self.generate_snippets_guidelines_page(site)
-      page = Page.new(site, File.join('snippets', 'guidelines'), 'index.html',
-        'snippets_guidelines.html', 'Snippets Guidelines')
       site.pages << page
     end
   end
