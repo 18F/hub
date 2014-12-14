a7f12fe6b43e479caba5a910f95748cdc91d9ee1
Mike Bland <michael.bland@gsa.gov>
2014-12-14 10:38:44 -0500

    Replace plugin-generated departments/ w/ Markdown

--- _plugins/departments.rb
+++ _plugins/departments.rb
@@ -4,7 +4,6 @@ module Hub
       return unless site.data.member? 'departments'
       departments = site.data['departments']
       departments.each {|dept| generate_department_page(site, dept)}
-      generate_department_index_page(site, departments)
     end
 
     def self.generate_department_page(site, department)
@@ -12,13 +11,6 @@ module Hub
       page = Page.new(site, File.join('departments', canonicalized_name),
         'index.html', 'department.html', department['name'])
       page.data['department'] = department
-      site.pages << page
-    end
-
-    def self.generate_department_index_page(site, departments)
-      page = Page.new(site, 'departments', 'index.html',
-        'department_index.html', 'Departments')
-      page.data['departments'] = departments
       site.pages << page
     end
   end
