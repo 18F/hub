1b03ca93f35211fbf583a0a7c51f48c247a4a255
Mike Bland <michael.bland@gsa.gov>
2014-12-14 10:57:04 -0500

    Replace plugin-generated snippets/ with Markdown

--- _plugins/snippets.rb
+++ _plugins/snippets.rb
@@ -5,7 +5,6 @@ module Hub
       site.data['snippets'].each do |timestamp, snippets|
         generate_snippets_page(site, timestamp, snippets)
       end
-      generate_snippets_index_page(site)
     end
 
     def self.generate_snippets_page(site, timestamp, snippets)
@@ -13,20 +12,6 @@ module Hub
         "snippets.html",
         "Snippets for #{Canonicalizer.hyphenate_yyyymmdd(timestamp)}")
       page.data['snippets'] = snippets
-      site.pages << page
-    end
-
-    def self.generate_snippets_index_page(site)
-      page = Page.new(site, 'snippets', 'index.html', 'snippets_index.html',
-        'Snippets')
-
-      snippets = []
-      site.data['snippets'].each do |t,s|
-        snippets << {'timestamp'=>t, 'snippets'=>s}
-      end
-
-      page.data['snippets'] = snippets
-      page.data['members'] = site.data['snippets_team_members']
       site.pages << page
     end
   end
---
+++ pages/snippets.md
@@ -0,0 +1,14 @@
+---
+permalink: snippets/
+---
+# Snippets
+<br/>
+**[Guidelines for Writing Snippets]({{ site.baseurl }}/snippets/guidelines)**
+
+## By Date
+{% for s in site.data.snippets reversed %}{% assign timestamp = s[0] %}{% assign snippets = s[1] %}
+**[{{ timestamp | hyphenate_yyyymmdd }}]({{ site.baseurl }}/snippets/{{ timestamp }})**: {{ snippets | size }} snippets<br/>{% endfor %}
+
+##By Team Member
+{% for m in site.data.snippets_team_members %}
+[{{ m.full_name }}]({{ site.baseurl }}/snippets/{{ m.name }})<br/>{% endfor %}
