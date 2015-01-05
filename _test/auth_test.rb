703376348b0d738710da48ca7d5fcce91bf0b8b3
Mike Bland <michael.bland@gsa.gov>
2015-01-05 18:00:57 -0500

    Add license info to source files

--- _test/auth_test.rb
+++ _test/auth_test.rb
@@ -1,3 +1,19 @@
+# 18F Hub - Docs & connections between team members, projects, and skill sets
+#
+# Written in 2014 by Mike Bland (michael.bland@gsa.gov)
+# on behalf of the 18F team, part of the US General Services Administration:
+# https://18f.gsa.gov/
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
+# @author Mike Bland (michael.bland@gsa.gov)
+
 require_relative "../_plugins/auth"
 require_relative "page"
 
