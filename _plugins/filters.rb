b76e660b6a483438c29faf7e360f812ce1ceebff
Aidan Feldman <aidan.feldman@gsa.gov>
2015-01-21 02:44:40 -0500

    remove leading and trailing whitespace when condensing

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -19,7 +19,7 @@ module Hub
     # https://github.com/Shopify/liquid/blob/v2.6.1/lib/liquid/standardfilters.rb#L71-L74
     # to replace newlines with spaces.
     def condense(input)
-      input.to_s.gsub(/\r?\n/, ' '.freeze)
+      input.to_s.gsub(/\r?\n/, ' '.freeze).strip
     end
   end
 end
