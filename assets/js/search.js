0faf2185656626498920e1fb2490302f4858b29e
Aidan Feldman <aidan.feldman@gsa.gov>
2015-01-25 19:44:32 -0500

    simplify search code

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,22 +1,10 @@
 var ngHub = angular.module('hubSearch', []);
 
-// http://andyshora.com/promises-angularjs-explained-as-cartoon.html
 ngHub.factory('pages', function($http, $q) {
   return {
     get: function() {
       // TODO fix against site.baseurl
-      return $http.get('api/v1/pages.json').
-        then(function(response) {
-          if (angular.isObject(response.data)) {
-            return response.data;
-          } else {
-            // invalid response
-            return $q.reject(response.data);
-          }
-        }, function(response) {
-          // something went wrong
-          return $q.reject(response.data);
-        });
+      return $http.get('api/v1/pages.json');
     }
   };
 });
@@ -25,7 +13,7 @@ ngHub.factory('pages', function($http, $q) {
 ngHub.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
 
 ngHub.controller('SearchController', ['$scope', 'pages', function($scope, Pages) {
-  Pages.get().then(function(data) {
-    $scope.pages = data.entries;
+  Pages.get().then(function(response) {
+    $scope.pages = response.data.entries;
   });
 }]);
