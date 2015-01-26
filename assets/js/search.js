a3fca59a295d0e78edfe013aa65b4289abff1d9d
Aidan Feldman <aidan.feldman@gsa.gov>
2015-01-25 21:27:25 -0500

    search using lunrjs

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,19 +1,61 @@
 var ngHub = angular.module('hubSearch', []);
 
 ngHub.factory('pages', function($http, $q) {
+  // TODO fix against site.baseurl
+  var promise = $http.get('api/v1/pages.json');
   return {
     get: function() {
-      // TODO fix against site.baseurl
-      return $http.get('api/v1/pages.json');
+      return promise;
     }
+  };
+});
+
+ngHub.factory('pagesByUrl', function(pages) {
+  var result = {};
+
+  pages.get().then(function(response) {
+    var docs = response.data.entries;
+    angular.forEach(docs, function(doc) {
+      result[doc.url] = doc;
+    });
+  });
+
+  return result;
+});
+
+ngHub.factory('pageIndex', function(pages) {
+  var index = lunr(function() {
+    this.field('title', {boost: 10})
+    this.field('body')
+    this.ref('url');
+  });
+
+  pages.get().then(function(response) {
+    var docs = response.data.entries;
+    angular.forEach(docs, function(page) {
+      index.add(page);
+    });
+  });
+
+  return index;
+});
+
+ngHub.factory('pagesSearch', function($filter, pagesByUrl, pageIndex) {
+  return function(term) {
+    var results = pageIndex.search(term);
+    angular.forEach(results, function(result) {
+      var page = pagesByUrl[result.ref];
+      result.page = page;
+    });
+    return results;
   };
 });
 
 // http://stackoverflow.com/a/19705096/358804
 ngHub.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
 
-ngHub.controller('SearchController', ['$scope', 'pages', function($scope, Pages) {
-  Pages.get().then(function(response) {
-    $scope.pages = response.data.entries;
+ngHub.controller('SearchController', ['$scope', 'pagesSearch', function($scope, pagesSearch) {
+  $scope.$watch('searchText', function() {
+    $scope.results = pagesSearch($scope.searchText);
   });
 }]);
