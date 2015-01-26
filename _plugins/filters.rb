bd5bca0af75e5ebb595fe463074dc2b7ce161674
Aidan Feldman <aidan.feldman@gsa.gov>
2015-01-26 17:58:15 -0500

    use jekyll_pages_api gem
    copy in files from
    https://github.com/18F/hub/commit/a4b94933d36b57f7d57164ccbaebff4546623c89

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -30,13 +30,6 @@ module Hub
     def canonicalize(s)
       Canonicalizer.canonicalize s
     end
-
-    # Slight tweak of
-    # https://github.com/Shopify/liquid/blob/v2.6.1/lib/liquid/standardfilters.rb#L71-L74
-    # to replace newlines with spaces.
-    def condense(input)
-      input.to_s.gsub(/\r?\n/, ' '.freeze).strip
-    end
   end
 end
 
