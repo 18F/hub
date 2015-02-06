290c30f170ceda37d57df49e2e0e7ae4758e6db4
Shawn Allen <shawn.allen@gsa.gov>
2015-02-06 11:14:28 -0800

    clean up code, add some helpful comments

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,6 @@
 (function(exports) {
 
+  // our DOM container and status message
   var map = d3.select("#team-map")
         .classed("loading", true),
       status = map.append("p")
@@ -19,130 +20,185 @@
     {code: "AUS", label: "Austin", location: [-97.669889, 30.194528]}
   ];
 
+  // URLs to grab
   var urls = {
     team: "/api/team/api.json",
     topology: "/assets/data/us-states.json"
   };
 
+  // set up map size, projection, and <svg> container
+  var width = 1100,
+      height = 600,
+      proj = d3.geo.albersUsa()
+        .scale(1285)
+        .translate([width / 2, height / 2]),
+      svg = map.append("svg")
+        .attr("class", "map")
+        .attr("viewBox", [0, 0, width, height].join(" "));
+
+  // queue() loads the URLs in parallel, rather than
+  // using nested callbacks
   queue()
     .defer(d3.json, urls.team)
     .defer(d3.json, urls.topology)
-    .await(function(error, team, topology) {
-      map.classed("loading", false);
-      if (error) return showError(error.statusText);
+    .await(allDone);
 
-      status.text("");
+  function allDone(error, team, topology) {
+    map.classed("loading", false);
+    if (error) return showError(error.statusText);
 
-      var states = topojson.feature(topology, topology.objects.states)
-        .features;
+    // kill the status message
+    status.remove();
 
-      var members = d3.values(team)
-            .filter(function(d) { return d.location; }),
-          byLocation = d3.nest()
-            .key(function(d) { return d.location; })
-            .map(members);
+    // get a GeoJSON FeatureCollection from the topology
+    var states = topojson.feature(topology, topology.objects.states)
+      .features;
+    // draw the states background
+    renderMapBackground(states);
 
-      locations.forEach(function(d) {
-        d.members = byLocation[d.code] || [];
+    // group members by location
+    var members = d3.values(team)
+          .filter(function(d) { return d.location; }),
+        byLocation = d3.nest()
+          .key(function(d) { return d.location; })
+          .map(members);
+
+    // then assign a list of members to each location
+    locations.forEach(function(d) {
+      d.members = byLocation[d.code] || [];
+    });
+
+    // then draw them on the map
+    renderLocations(locations);
+  }
+
+  /*
+   * takes an array of GeoJSON features and draws them
+   * into a background <g> using the projection `proj`.
+   */
+  function renderMapBackground(features) {
+    var g = svg.append("g")
+        .attr("class", "states background"),
+      path = d3.geo.path()
+        .projection(proj);
+
+    feature = g.selectAll(".state")
+      .data(features)
+      .enter()
+      .append("path")
+        .attr("class", "state")
+        .attr("d", path);
+  }
+
+  /*
+   * takes a list of locations that look like:
+   *
+   * {
+   *   code: "SFO",
+   *   location: [<longitude>, <latitude>],
+   *   members: []
+   * }
+   *
+   * and draws them on the map as circles with radii proportional to
+   * their member count.
+   */
+  function renderLocations(locations) {
+    // size accessor (keeping things DRY)
+    var size = function getSize(d) {
+          return d.members.length;
+        },
+        // label accessor
+        label = function getLabel(d) {
+          var n = size(d),
+              s = n === 1 ? "" : "s";
+          return [d.code, ": ", size(d), " member" + s].join("");
+        },
+        // radius is a sqrt scale because a circle's area
+        // is proportional to the square root of its radius
+        radius = d3.scale.sqrt()
+          .domain([1, d3.max(locations, size)])
+          .rangeRound([10, 40])
+          .clamp(true);
+
+    // create a <g> container for the pins, and a <g.pin>
+    // for each location
+    var pins = svg.append("g")
+          .attr("class", "pins"),
+        pin = pins.selectAll(".pin")
+          .data(locations)
+          .enter()
+          .append("g")
+            .attr("class", "pin")
+            .attr("transform", function(d) {
+              // translate the pin to its projected coordinate
+              var p = proj(d.location).map(Math.round);
+              return "translate(" + p + ")";
+            }),
+        // wrap an anchor around each circle
+        link = pin.append("a")
+          .attr("xlink:href", function(d) {
+            return "#" + d.code;
+          }),
+        // and draw a circle with a proportional radius
+        circle = link.append("circle")
+          .attr("aria-label", label)
+          .attr("r", function(d) {
+            // save the radius for later use
+            return d.radius = radius(size(d));
+          });
+
+    // sort the pins (in the DOM) by latitude
+    pin.sort(defaultSort);
+
+    // append a tooltip <g> to each pin
+    var tip = pin.append("g")
+      .attr("class", "tip")
+      .attr("transform", function(d) {
+        // move it to the top of its circle's radius
+        return "translate(" + [0, -d.radius] + ")";
       });
 
-      var width = 1100,
-          height = 600,
-          proj = d3.geo.albersUsa()
-            .scale(1285)
-            .translate([width / 2, height / 2]),
-          path = d3.geo.path()
-            .projection(proj);
+    // then render a tooltip on each pin
+    tip.call(tooltip()
+      .text(label));
 
-      var svg = map.append("svg")
-            .attr("class", "map")
-            .attr("viewBox", [0, 0, width, height].join(" ")),
-          g = svg.append("g")
-            .attr("class", "states background"),
-          feature = g.selectAll(".state")
-            .data(states)
-            .enter()
-            .append("path")
-              .attr("class", "state")
-              .attr("d", path);
+    // activate and deactiveate event handlers toggle the "on" class
+    var activate = function activate(d) {
+          this.classList.add("on");
+          // move this node to the front
+          this.parentNode.appendChild(this);
+        },
+        deactivate = function deactivate(d) {
+          this.classList.remove("on");
+          // reset the sort order (in the DOM)
+          pin.sort(defaultSort);
+        };
 
-      var size = function(d) { return d.members.length; },
-          radius = d3.scale.sqrt()
-            .domain([1, d3.max(locations, size)])
-            .rangeRound([10, 40])
-            .clamp(true);
-
-      var pins = svg.append("g")
-            .attr("class", "pins"),
-          pin = pins.selectAll(".pin")
-            .data(locations)
-            .enter()
-            .append("g")
-              .attr("class", "pin")
-              .attr("transform", function(d) {
-                var p = proj(d.location).map(Math.round);
-                return "translate(" + p + ")";
-              }),
-          link = pin.append("a")
-            .attr("xlink:href", function(d) {
-              return "#" + d.code;
-            }),
-          circle = link.append("circle")
-            .attr("aria-label", label)
-            .attr("r", function(d) {
-              return d.radius = radius(size(d));
-            });
-
-      pin.sort(defaultSort);
-
-      var tip = pin.append("g")
-        .attr("class", "tip")
-        .attr("transform", function(d) {
-          return "translate(" + [0, -d.radius] + ")";
-        })
-        .call(tooltip()
-          .text(label));
-
-      function label(d) {
-        var n = size(d),
-            s = n === 1 ? "" : "s";
-        return [d.code, ": ", size(d), " member" + s].join("");
-      }
-
-      pin
-        .on("mouseover", activate)
-        .on("mouseout", deactivate)
-        .on("focus", activate)
-        .on("blur", deactivate);
-
-      function activate(d) {
-        this.classList.add("on");
-        this.parentNode.appendChild(this);
-      }
-
-      function deactivate(d) {
-        this.classList.remove("on");
-        pin.sort(defaultSort);
-      }
-
-      function defaultSort(a, b) {
-        return d3.descending(a.radius, b.radius)
-            || d3.descending(a.location[1], b.location[1]);
-      }
-    });
+    // add handlers for mouse and focus events
+    pin
+      .on("mouseover", activate)
+      .on("mouseout", deactivate)
+      .on("focus", activate)
+      .on("blur", deactivate);
+  }
 
   function showError(error) {
     map.classed("error", true);
     status.text("Error: " + error);
   }
 
-  function rebind(fn, context) {
-    var args = Array.prototype.slice.call(arguments, 2);
-    return function bound() {
-      return fn.apply(context, args);
-    };
+  function defaultSort(a, b) {
+    return d3.descending(a.radius, b.radius)
+        || d3.descending(a.location[1], b.location[1]);
   }
 
+  /*
+   * A simple little tooltip renderer:
+   *
+   * var tip = tooltip()
+   *  .text(function(d) { return d.title; });
+   * svg.call(tip);
+   */
   function tooltip() {
     var textOffset = 8,
         text = "",
