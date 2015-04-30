71e82e3a9c5aa83df1178bfe190d8a71989aac09
Mike Bland <michael.bland@gsa.gov>
2015-04-30 15:45:29 -0400

    Add skip_index capability
    Developers can set `skip_index: true` in `_config.yml` to rebuild the site
    more quickly by skipping search indexing.

--- _plugins/search_hook.rb
+++ _plugins/search_hook.rb
@@ -11,6 +11,7 @@ module Jekyll
 
     def after_render
       pages_api_after_render
+      return if self.config['skip_index']
       index = Hub::SearchIndexBuilder.build_index(self)
       self.pages << index if index != nil
     end
