d87c1b9dceba3c9e7c44d959aec069eeec2aeaca
Catherine Devlin <catherine.devlin@gsa.gov>
2016-02-02 15:47:57 -0500

    Add test for photo_or_placeholder

--- set_hmac_auth.sample.sh
+++ set_hmac_auth.sample.sh
@@ -1,12 +1,4 @@
 # To access the private data on the team-api server,
 # get the HMAC shared secret and set it in the following
-# environment variables ("source" this file).
-export TEAM_RAW_HMAC=<shared_secret>
-export LOCATIONS_RAW_HMAC=<shared_secret>
-export PROJECTS_RAW_HMAC=<shared_secret>
-export DEPARTMENTS_RAW_HMAC=<shared_secret>
-export WORKING_GROUPS_RAW_HMAC=<shared_secret>
-export GUILDS_RAW_HMAC=<shared_secret>
-export SKILLS_RAW_HMAC=<shared_secret>
-export INTERESTS_RAW_HMAC=<shared_secret> 
-export SNIPPETS_RAW_HMAC=<shared_secret> 
+# environment variable ("source" this file).
+export HMAC_KEY=<shared_secret>
